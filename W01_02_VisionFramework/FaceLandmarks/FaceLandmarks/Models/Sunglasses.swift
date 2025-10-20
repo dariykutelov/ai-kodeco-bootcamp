7bccfd1810e97cac244fb0c1fbf69e9c004dffbb
dariykutelov <dariy@kutelov.com>
2025-10-20 18:02:20 +0300

    Adjust sunglasses verticalTopToNoseOffset values

--- W01_02_VisionFramework/FaceLandmarks/FaceLandmarks/Models/Sunglasses.swift
+++ W01_02_VisionFramework/FaceLandmarks/FaceLandmarks/Models/Sunglasses.swift
@@ -17,12 +17,12 @@ extension SunglassImage {
         SunglassImage(imageName: "sunglasses-1", verticalTopToNoseOffset: 18),
         SunglassImage(imageName: "sunglasses-2", verticalTopToNoseOffset: 17),
         SunglassImage(imageName: "sunglasses-3", verticalTopToNoseOffset: 16.6),
-        SunglassImage(imageName: "sunglasses-4", verticalTopToNoseOffset: 30),
-        SunglassImage(imageName: "sunglasses-5", verticalTopToNoseOffset: 25),
-        SunglassImage(imageName: "sunglasses-6", verticalTopToNoseOffset: 26),
+        SunglassImage(imageName: "sunglasses-4", verticalTopToNoseOffset: 17),
+        SunglassImage(imageName: "sunglasses-5", verticalTopToNoseOffset: 22),
+        SunglassImage(imageName: "sunglasses-6", verticalTopToNoseOffset: 23),
         SunglassImage(imageName: "sunglasses-7", verticalTopToNoseOffset: 15.2),
-        SunglassImage(imageName: "sunglasses-8", verticalTopToNoseOffset: 23.2),
-        SunglassImage(imageName: "sunglasses-9", verticalTopToNoseOffset: 33),
+        SunglassImage(imageName: "sunglasses-8", verticalTopToNoseOffset: 19),
+        SunglassImage(imageName: "sunglasses-9", verticalTopToNoseOffset: 18),
         SunglassImage(imageName: "sunglasses-10", verticalTopToNoseOffset: 21.2)
     ]
 }
