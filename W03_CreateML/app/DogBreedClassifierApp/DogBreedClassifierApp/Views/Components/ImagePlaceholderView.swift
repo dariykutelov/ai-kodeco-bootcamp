ea9d665283b246996b63089a1ac09d19f0975907
dariykutelov <dariy@kutelov.com>
2025-10-29 19:08:04 +0200

    Style home view

---
+++ W03_CreateML/app/DogBreedClassifierApp/DogBreedClassifierApp/Views/Components/ImagePlaceholderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePlaceholderView.swift
+//  DogBreedClassifierApp
+//
+//  Created by Dariy Kutelov on 29.10.25.
+//
+
+import SwiftUI
+
+struct ImagePlaceholderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePlaceholderView()
+}
