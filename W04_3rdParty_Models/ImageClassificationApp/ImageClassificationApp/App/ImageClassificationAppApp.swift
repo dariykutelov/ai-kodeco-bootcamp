e05432959788fb4f09ccc0429c2e0d12f1254c6b
dariykutelov <dariy@kutelov.com>
2025-11-07 17:06:29 +0200

    Yolo model generator script

---
+++ W04_3rdParty_Models/ImageClassificationApp/ImageClassificationApp/App/ImageClassificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationAppApp.swift
+//  ImageClassificationApp
+//
+//  Created by Dariy Kutelov on 7.11.25.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
