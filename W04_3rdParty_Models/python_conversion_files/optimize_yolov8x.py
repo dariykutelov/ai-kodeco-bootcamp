ad0e2e982a0a93a8e17930345ccec8caeb870117
dariykutelov <dariy@kutelov.com>
2025-11-08 10:03:35 +0200

    Add yolov 8int

--- W04_3rdParty_Models/python_conversion_files/optimize_yolov8x.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8x-cls.pt")
-
-model.export(format="coreml", int8=True, imgsz=(224, 224), name="yolov8x-cls-8")
-print("Optimized YOLOv8x-cls model (int8) successfully exported to CoreML format as yolov8x-cls-8.mlpackage")
-
